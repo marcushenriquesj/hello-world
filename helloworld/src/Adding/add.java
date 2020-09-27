1d1ba78547bfeba7a88c623074eb5ad6b36f5a9c
marcu <marcu@192.168.1.211>
2020-09-27 19:39:31 -0400

    This has the system print my name,email,id as well as adding two integers and a integer +1

---
+++ helloworld/src/Adding/add.java
@@ -0,0 +1,10 @@
+package Adding;
+
+public class add {
+
+	public static void main(String[] args) {
+		// this will add the numbers 25 and 50
+		System.out.println(75+25);
+	}
+
+}
---
+++ helloworld/src/numberplusone/addplus1.java
@@ -0,0 +1,10 @@
+package numberplusone;
+
+public class addplus1 {
+
+	public static void main(String[] args) {
+		//this will add the integer 10 +1
+		System.out.println(10+1);
+	}
+
+}
